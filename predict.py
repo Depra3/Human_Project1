3277bcef26340395cadb5ffd87ee2cd708772d45
Depar3 <parkmin0318@gmail.com>
2023-02-09 16:19:53 +0900

    updated

--- predict.py
+++ predict.py
@@ -22,15 +22,14 @@ warnings.filterwarnings("ignore")
 from stqdm_model import stqdm_model
 
 def run_predict():
-    st.title("전세 예측:상승세인_차트:")
     st.markdown("""
     *※ 왼쪽 사이드바에 원하시는 메뉴를 선택하세요 ※*
     """)
-    nRe = 0
+    
     df = pd.read_csv('data/bds_data.csv', encoding='cp949')
     df_copy = df.copy()
     data = pd.read_csv('data/bds_data.csv', encoding='cp949')
-    sub_menu = ['전월세 월평균 그래프', '전월세 실거래수 지역 순위', '날짜별 거래', '전세예측']
+    sub_menu = ['전월세 월평균 그래프', '전월세 실거래수 지역 순위', '날짜별 거래', '전세예측', '전월세 전환율/대출이자 계산기']
     sub_choice = st.sidebar.selectbox("메뉴", sub_menu)
 
     now = datetime.now()
@@ -109,7 +108,7 @@ def run_predict():
                     else:
                         ws = pd.concat([ws , wm])
                 st.write(ws)
-                
+    
     elif sub_choice == '전월세 실거래수 지역 순위':
         t1, t2 = st.tabs(['월세', '전세'])
         with t1:
@@ -182,12 +181,16 @@ def run_predict():
         else:
             st.markdown('# 금일 거래는 없습니다.')
             st.plotly_chart(fig)
-    else:
-        # st.subheader("전세예측")
-        # st.markdown('# 진행중~')
-        # t1, t2 = 
+    elif sub_choice == '전세 예측':
 
-        st.write("### 전세 -> 월세")
+        st.subheader("전세예측")
+        st.markdown('# 진행중~')
+        
+    elif sub_choice == '전월세 전환율/대출이자 계산기':
+        # 전월세 전환율 계산기 / 이자 계산
+        st.subheader('전월세 전환율 계산기')
+        st.markdown('***')
+        st.write("#### 전세 -> 월세")
         c1, c2, c3 = st.columns([1,1,1])
 
         p1 = c1.empty()
@@ -196,18 +199,21 @@ def run_predict():
         with p1.container():
             n1 = st.number_input("전월세 전환율 (%)", step=0.1)
         with p2.container():
-            n2 = st.number_input("월세 보증금 (만원)", step=1)
+            n2 = st.number_input("월세 보증금 (만원)", step=0.1)
         with p3.container():
-            n3 = st.number_input("전세 보증금 (만원)", step=1)
-            nRe = ((n3-n2)*n1)/1200
-
-        n4 = st.number_input("월세 (만원)", step=0.1, value=nRe)
+            n3 = st.number_input("전세 보증금 (만원)", step=0.1)
+        nRe = ((n3-n2)*(n1/100))/12
+        if nRe <= 0:
+            nRe = 0
+        # n4 = st.number_input("월세 (만원)", step=0.1, value=float(nRe))
+        st.write('월세(만원)')
+        st.success(str(f'{nRe:.2f}') + '만원')
         p1 = st.empty()
         p2 = st.empty()
         p3 = st.empty()
 
-        st.write('#   ')
-        st.write("### 월세 -> 전세")
+        st.markdown('***')
+        st.write("#### 월세 -> 전세")
         c4, c5, c6 = st.columns([1,1,1])
         p4 = c4.empty()
         p5 = c5.empty()
@@ -215,30 +221,122 @@ def run_predict():
         with p4.container():
             u1 = st.number_input("전월세 전환율 (%) ", step=0.1)
         with p5.container():
-            u2 = st.number_input("월세 보증금 (원) ", step=1)
+            u2 = st.number_input("월세 보증금 (만원) ", step=0.1)
         with p6.container():
-            u3 = st.number_input("월세 (원) ", step=0.1)
-        u4 = st.number_input("전세 보증금 (원) ", step=1)
+            u3 = st.number_input("월세 (만원) ", step=0.1)
+
+        if u1 == 0:
+            uRe = 0
+        else:
+            uRe = ((u3*12)/(u1/100)) + u2
+
+        # u4 = st.number_input("전세 보증금 (만원) ", step=0.1, value=float(uRe))
+        st.write('전세 보증금 (만원)')
+        st.success(str(f'{uRe:.2f}') + '만원')
         p4 = st.empty()
         p5 = st.empty()
         p6 = st.empty()
 
-        st.write('#   ')
-        st.write("### 전환율 계산")
+        st.markdown('***')
+        st.write("#### 대출 이자 계산")
+        e = st.selectbox('상환 방법', ['원리금균등상환', '원금균등상환', '원금만기일시상환'])
         c7, c8, c9 = st.columns([1,1,1])
         p7 = c7.empty()
         p8 = c8.empty()
         p9 = c9.empty()
-        with p7.container():
-            m1 = st.number_input("월세 보증금 (원)  ", min_value=0, step=1)
-        with p8.container():
-            m2 = st.number_input("전세 보증금 (원)  ", min_value=0, step=1)
-        with p9.container():
-            m3 = st.number_input("월세 (원)  ", step=0.1)
-        m4 = st.number_input("전월세 전환율 (%)  ", step=0.1)
-        p7 = st.empty()
-        p8 = st.empty()
-        p9 = st.empty()
+        
+        with p7:
+            e1 = st.number_input('대출 금액(원)', step=1)
+        with p8:
+            e2 = st.number_input('대출 금리(연 %)', step=0.1)
+        with p9:
+            e3 = st.number_input('대출 기간(개월)', step=1)
+        if e == '원리금균등상환':
+            R = e2/1200
+            N = (1+R)**e3
+            if (N-1) <= 0:
+                eRe1 = 0
+                eRe2 = 0
+            else:
+                eRe1 = (e1*R*N)/(N-1)
+                eRe2 = 0
+        elif e == '원금균등상환':
+            eRe1 = e1*(e2/100)*((e3+1)/24)
+            eRe2 = eRe1/e3
+        else:
+            eRe1 = e1*(e2/1200)*e3
+            eRe2 = eRe1/e3
+        eRe1 = float(eRe1)
+        if e == '원리금균등상환':
+            # e5 = st.number_input('매월 상환금 (원금 + 이자) (원)', step=0.1, value=float(eRe1))
+            st.write('매월 상환금 (원금 + 이자)')
+            st.success(str(f'{eRe1:.2f}') + '만원')
+        else:
+            ce1, ce2 = st.columns([1,1])
+            pe1 = ce1.empty()
+            pe2 = ce2.empty()
+            with pe1:
+                # e5 = st.number_input('총 이자 금액', step=0.1, value=float(eRe1))
+                st.write('총 이자 금액')
+                st.success(str(f'{eRe1:.0f}') + '원')
+            with pe2:
+                # e6 = st.number_input('월별 이자 금액', step=0.1, value=float(eRe2))
+                st.write('월별 이자 금액')
+                st.success(str(f'{eRe2:.0f}') + '원')
+            p7 = st.empty()
+            p8 = st.empty()
+            p9 = st.empty()
+
+        st.markdown('***')
+        st.write("#### 전환율 계산")
+        c11, c12, c13 = st.columns([1,1,1])
+        p11 = c11.empty()
+        p12 = c12.empty()
+        p13 = c13.empty()
+        with p11.container():
+            m1 = st.number_input("전세 보증금 (만원)  ", step=0.1)
+        with p12.container():
+            m2 = st.number_input("월세 보증금 (만원)  ", step=0.1)
+        with p13.container():
+            m3 = st.number_input("월세 (만원)  ", step=0.1)
+        
+        if (m1-m2) == 0:
+            mRe = 0
+        else:
+            mRe = ((m3*12)/(m1-m2))*100
+        # m4 = st.number_input("전월세 전환율 (%)  ", step=0.1, value=float(mRe))
+        st.write('전월세 전환율 (%)')
+        st.success(str(f'{mRe:.2f}') + '%')
+        p11 = st.empty()
+        p12 = st.empty()
+        p13 = st.empty()
         
         
        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
